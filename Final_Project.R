3d9fdb34d4976292a454f21a1f41a303ecdf97e1
Jeremy Rodriguez <113404239+jrodriguez25@users.noreply.github.com>
2024-02-24 15:20:26 -0500

    network graph = filtered by pop>70 and no classic/jazz

--- Final_Project.R
+++ Final_Project.R
@@ -1,3 +1,6 @@
+
+
+
 library(igraph)
 library(tidyverse)
 
@@ -5,38 +8,45 @@ nodes <- read_csv("SpotifyNetworkNodes.csv")
 edges <- read_csv("SpotifyNetworkEdges.csv")
 
 
+#remove duplicates
+nodes <- nodes %>% distinct(spotify_id, .keep_all = TRUE)
+
 nodes$genres <- gsub("\\[", "", nodes$genres)
 nodes$genres <- gsub("\\]", "", nodes$genres)
 
 nodes$chart_hits <- gsub("\\]", "", nodes$chart_hits)
 nodes$chart_hits <- gsub("\\]", "", nodes$chart_hits)
 
+genres=nodes %>% 
+  separate_rows(genres, sep = "'") %>% 
+  count(genres)
+
+# filter the genres -------------------------------------------------------
+nodes_filtered <- nodes %>% 
+  filter(popularity>70) %>% 
+  filter(!str_detect(genres, "classic") & !str_detect(genres, "jazz"))
+
+
 
 
 # Get the unique Spotify IDs from nodes
-node_ids <- unique(nodes$spotify_id)
+node_ids <- unique(nodes_filtered$spotify_id)
 
 # Identify edges to remove (any edge where either id_0 or id_1 is not in node_ids)
 edges_to_remove <- !(edges$id_0 %in% node_ids) | !(edges$id_1 %in% node_ids)
 
-#remove ids not in "nodes"
 cleaned_edges <- edges[!edges_to_remove, ]
 
-network_graph <- graph_from_data_frame(d = cleaned_edges, vertices = nodes, directed = FALSE)
 
+network_graph <- graph_from_data_frame(d = cleaned_edges, vertices = nodes_filtered, directed = FALSE)
 
-plot(network_graph)
-#Next Steps
+network_simplified <- simplify(network_graph)
 
-#Combine nodes and edges into one dataset
-#clean chart hits
-#filter to top 600-800
-#filter to pop and rap 
+# gettign largest component -----------------------------------------------
+decomp = components(network_simplified)
 
-#centrality tests
-#structural similarity
-#bridges #edge betweeness 
+largest_comp = which.max(decomp$csize)
+large_ids = V(network_simplified)[decomp$membership == largest_comp]
+giant_comp = induced_subgraph(network_simplified, large_ids)
 
-
-
-
+plot(giant_comp)
