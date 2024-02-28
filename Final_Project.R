45f57181c1bf0f871fa957b17f804634628bcc19
Jeremy Rodriguez <113404239+jrodriguez25@users.noreply.github.com>
2024-02-27 23:09:54 -0500

    Update Final_Project.R

--- Final_Project.R
+++ Final_Project.R
@@ -42,11 +42,56 @@ network_graph <- graph_from_data_frame(d = cleaned_edges, vertices = nodes_filte
 
 network_simplified <- simplify(network_graph)
 
-# gettign largest component -----------------------------------------------
+# getting largest component -----------------------------------------------
 decomp = components(network_simplified)
-
 largest_comp = which.max(decomp$csize)
 large_ids = V(network_simplified)[decomp$membership == largest_comp]
 giant_comp = induced_subgraph(network_simplified, large_ids)
 
 plot(giant_comp)
+
+# Some Centrality Tests ---------------------------------------------------
+
+which.max(degree(giant_comp))
+degree(giant_comp)[699] #degree of 107
+
+betweenness(giant_comp, normalized = T)
+betweenness(giant_comp)
+
+which.max(closeness(giant_comp))
+
+which.max(eigen_centrality(giant_comp)$vector)
+
+jaccard_sim <- similarity(giant_comp, method = "jaccard")
+
+
+
+# Structural Similarity ---------------------------------------------------
+
+fg <- cluster_fast_greedy(giant_comp)
+plot(fg, giant_comp)
+
+layout <- layout_with_eigen(giant_comp)
+
+
+# Reorganizing the Dataset an Alternate Way -------------------------------
+
+edges_with_nodes_0 <- merge(cleaned_edges, nodes_filtered, by.x = "id_0", by.y ="spotify_id", all.x = TRUE)
+
+names(edges_with_nodes_0)[names(edges_with_nodes_0) == "name"] <- "name_0"
+names(edges_with_nodes_0)[names(edges_with_nodes_0) == "followers"] <- "followers_0"
+names(edges_with_nodes_0)[names(edges_with_nodes_0) == "popularity"] <- "popularity_0"
+names(edges_with_nodes_0)[names(edges_with_nodes_0) == "genres"] <- "genres_0"
+
+final_edges_with_nodes <- merge(edges_with_nodes_0, nodes, by.x = "id_1", by.y = "spotify_id", all.x = TRUE)
+
+names(final_edges_with_nodes)[names(final_edges_with_nodes) == "name"] <- "name_1"
+names(final_edges_with_nodes)[names(final_edges_with_nodes) == "followers"] <- "followers_1"
+names(final_edges_with_nodes)[names(final_edges_with_nodes) == "popularity"] <- "popularity_1"
+names(final_edges_with_nodes)[names(final_edges_with_nodes) == "genres"] <- "genres_1"
+
+
+final_df <- final_edges_with_nodes %>%
+  select(-id_0, -id_1) %>% # Remove ID columns
+  select(name_0, name_1, followers_0, followers_1, popularity_0, popularity_1, genres_0, genres_1) # Reorder columns
+
