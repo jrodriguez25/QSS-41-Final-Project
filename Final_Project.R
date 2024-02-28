2d70046a743960cf7184fa819ffedd7dfbe2369a
Jeremy Rodriguez <113404239+jrodriguez25@users.noreply.github.com>
2024-02-28 16:10:56 -0500

    updated after office hours

--- Final_Project.R
+++ Final_Project.R
@@ -2,8 +2,8 @@ library(igraph)
 library(tidyverse)
 library(disparityfilter)
 
-nodes <- read_csv("SpotifyNetworkNodes.csv")
-edges <- read_csv("SpotifyNetworkEdges.csv")
+edges <- read.csv("~//QSS41_proj/Data/edges.csv")
+nodes <- read.csv("~//QSS41_proj/Data/nodes.csv")
 
 
 #remove duplicates
@@ -47,6 +47,8 @@ giant_comp = induced_subgraph(network_graph_simplified, large_ids)
 
 # PRUNING   ----------------------------------------------------------------
 
+#Generate z1 – the normalized weight of each edge from the perspective of empl1
+inventors.simp$z1 = (inventors.simp$collaborations-inventors.simp$mean.collabs1)/inventors.simp$sd.collabs1
 
 # K-Core Composition ------------------------------------------------------
 network.core <- coreness(network_graph_simplified,  mode = "all")
@@ -65,8 +67,6 @@ network_simplified.weight = as.data.frame(cbind(get.edgelist(network_simplfied.g
 #rename
 names(network_simplified.weight) = c("artist1", "artist2", "weight")
 
-
-
 #Serrano Method Pruning NOT COMPLETE--------------------------------------------------
 
 #Serrano method
@@ -82,10 +82,49 @@ str(network_weights.b)
 
 
 # Flexible Backbone Pruning NOT COMPLETE -----------------------------------------------
+#paths of distance 2
+collabs.adj = get.adjacency(network_graph_simplified, sparse = F)
+collabs.2 = collabs.adj %*% collabs.adj
+diag(collabs.2) = 0
+collabs.2[collabs.adj == 0] = 0
+
+#Convert it to an edgelist
+collabs.g2 = graph.adjacency(collabs.2, weighted = TRUE)
+collabs.weight = as.data.frame(cbind(get.edgelist(collabs.g2), E(collabs.g2)$weight))
+
+#rename
+names(collabs.weight) = c("artist1", "artist2", "weight")
+collabs.weight$weight <- as.numeric(collabs.weight$weight) # make numeric
 
 
+#Merge the mean and sd of collaborations for each artist1 to the edgelist
+mean.collabs = aggregate(collabs.weight$weight, list(collabs.weight$artist1), FUN = mean)
+collabs.weight = merge(collabs.weight, mean.collabs, by.x = "artist1", by.y = "Group.1")
+names(collabs.weight)[4] = "mean.collabs1"
+sd.collabs = aggregate(collabs.weight$weight, list(collabs.weight$artist1), FUN = sd)
+collabs.weight = merge(collabs.weight, sd.collabs, by.x = "artist1", by.y = "Group.1")
+names(collabs.weight)[5] = "sd.collabs1"
 
+#Generate z1 – the normalized weight of each edge from the perspective of artist1
+collabs.weight$z1 = (collabs.weight$weight-collabs.weight$mean.collabs1)/collabs.weight$sd.collabs1
 
+#Same as for artist1, but for artist2
+mean.collabs = aggregate(collabs.weight$weight, list(collabs.weight$artist2), FUN = mean)
+collabs.weight = merge(collabs.weight, mean.collabs, by.x = "artist2", by.y = "Group.1")
+names(collabs.weight)[7] = "mean.collabs2"
+sd.collabs = aggregate(collabs.weight$weight, list(collabs.weight$artist2), FUN = sd)
+collabs.weight = merge(collabs.weight, sd.collabs, by.x = "artist2", by.y = "Group.1")
+names(collabs.weight)[8] = "sd.collabs2"
+
+#Generate z1 – the normalized weight of each edge from the perspective of artist1
+collabs.weight$z2 = (collabs.weight$weight-collabs.weight$mean.collabs2)/collabs.weight$sd.collabs2
+
+#We can now write our own rules in terms of z-scores. -> we need to mess around with these weight!!
+collabs.prune = collabs.weight[collabs.weight$z1 > 2 | collabs.weight$z2 > 2,]
+collabs.prune = collabs.prune[!is.na(collabs.prune$z1), 1:3]
+
+collabsgraph.prune <- graph.data.frame(collabs.prune)
+plot(collabsgraph.prune)
 # STRUCTURAL SIMILAIRITY--NOT COMPLETE ---------------------------------------------------
 
 
@@ -101,3 +140,9 @@ plot(fg, giant_comp)
 layout <- layout_with_eigen(giant_comp)
 
 
+#test: getting rid of NAs: dom't need rn storage spaceeeeeee
+collabs.weight$weight[!is.na(collabs.weight$weight)]
+collabs.weight$weight <- as.numeric(collabs.weight$weight)
+
+collabs.weight$artist1[!is.na(collabs.weight$artist1)]
+collabs.weight$artist2[!is.na(collabs.weight$artist2)]
