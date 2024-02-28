e2fc741fc0458bcce9e855e98067724f8f49d44b
Jeremy Rodriguez <113404239+jrodriguez25@users.noreply.github.com>
2024-02-28 00:49:10 -0500

    coreness pruning done. backbone method in progress

--- Final_Project.R
+++ Final_Project.R
@@ -1,8 +1,6 @@
-
-
-
 library(igraph)
 library(tidyverse)
+library(disparityfilter)
 
 nodes <- read_csv("SpotifyNetworkNodes.csv")
 edges <- read_csv("SpotifyNetworkEdges.csv")
@@ -26,54 +24,6 @@ nodes_filtered <- nodes %>%
   filter(popularity>70) %>% 
   filter(!str_detect(genres, "classic") & !str_detect(genres, "jazz"))
 
-
-
-
-# Get the unique Spotify IDs from nodes
-node_ids <- unique(nodes_filtered$spotify_id)
-
-# Identify edges to remove (any edge where either id_0 or id_1 is not in node_ids)
-edges_to_remove <- !(edges$id_0 %in% node_ids) | !(edges$id_1 %in% node_ids)
-
-cleaned_edges <- edges[!edges_to_remove, ]
-
-
-network_graph <- graph_from_data_frame(d = cleaned_edges, vertices = nodes_filtered, directed = FALSE)
-
-network_simplified <- simplify(network_graph)
-
-# getting largest component -----------------------------------------------
-decomp = components(network_simplified)
-largest_comp = which.max(decomp$csize)
-large_ids = V(network_simplified)[decomp$membership == largest_comp]
-giant_comp = induced_subgraph(network_simplified, large_ids)
-
-plot(giant_comp)
-
-# Some Centrality Tests ---------------------------------------------------
-
-which.max(degree(giant_comp))
-degree(giant_comp)[699] #degree of 107
-
-betweenness(giant_comp, normalized = T)
-betweenness(giant_comp)
-
-which.max(closeness(giant_comp))
-
-which.max(eigen_centrality(giant_comp)$vector)
-
-jaccard_sim <- similarity(giant_comp, method = "jaccard")
-
-
-
-# Structural Similarity ---------------------------------------------------
-
-fg <- cluster_fast_greedy(giant_comp)
-plot(fg, giant_comp)
-
-layout <- layout_with_eigen(giant_comp)
-
-
 # Reorganizing the Dataset an Alternate Way -------------------------------
 
 edges_with_nodes_0 <- merge(cleaned_edges, nodes_filtered, by.x = "id_0", by.y ="spotify_id", all.x = TRUE)
@@ -94,4 +44,74 @@ names(final_edges_with_nodes)[names(final_edges_with_nodes) == "genres"] <- "gen
 final_df <- final_edges_with_nodes %>%
   select(-id_0, -id_1) %>% # Remove ID columns
   select(name_0, name_1, followers_0, followers_1, popularity_0, popularity_1, genres_0, genres_1) # Reorder columns
+
+
+# Create the Graph --------------------------------------------------------
+
+# Get the unique Spotify IDs from nodes
+node_ids <- unique(nodes_filtered$spotify_id)
+
+# Identify edges to remove (any edge where either id_0 or id_1 is not in node_ids)
+edges_to_remove <- !(edges$id_0 %in% node_ids) | !(edges$id_1 %in% node_ids)
+cleaned_edges <- edges[!edges_to_remove, ]
+network_graph <- graph_from_data_frame(d = cleaned_edges, vertices = nodes_filtered, directed = FALSE)
+network_graph_simplified <- simplify(network_graph)
+
+# getting largest component -----------------------------------------------
+decomp = components(network_graph_simplified)
+largest_comp = which.max(decomp$csize)
+large_ids = V(network_simplified)[decomp$membership == largest_comp]
+giant_comp = induced_subgraph(network_graph_simplified, large_ids)
+
+
+
+# Pruning   ----------------------------------------------------------------
+
+#Weighting the edges for unweighted graphs
+
+network_simplified.adj = get.adjacency(network_graph_simplified, sparse = F)
+network_simplified.2 = network_simplified.adj %*% network_simplified.adj
+diag(network_simplified.2) = 0
+network_simplified.2[network_simplified.adj ==0]=0
+
+#Convert it to an edgelist
+network_simplfied.g2 = graph.adjacency(network_simplified.2, weighted = TRUE)
+network_simplified.weight = as.data.frame(cbind(get.edgelist(network_simplfied.g2), E(network_simplfied.g2)$weight))
+
+names(network_simplified.weight) = c("artist1", "artist2", "weight")
+
+
+# K-Core Composition ------------------------------------------------------
+network.core <- coreness(network_graph_simplified,  mode = "all")
+network_core_df <- data.frame(node = names(network.core), coreness = network.core)
+
+#rename
+
+#Serrano Method Pruning --------------------------------------------------
+
+#Serrano method
+network_weights.g = graph.edgelist(as.matrix(network_simplified.weight[,1:2]))
+E(network_weights.g)$weight <- as.numeric(network_simplified.weight[,3])
+
+E(network_weights.g)$weight
+
+network_weights.b = backbone(network_weights.g, weights = E(network_weights.g)$weight, alpha= .5)
+
+str(network_weights.b)
+
+
+
+# Flexible Backbone Pruning -----------------------------------------------
+
+mean.collabs = aggregate(network_simplified.weight$weight, list(network_simplified.weight$artist1), FUN = mean)
+
+
+
+# Structural Similarity ---------------------------------------------------
+
+fg <- cluster_fast_greedy(giant_comp)
+plot(fg, giant_comp)
+
+layout <- layout_with_eigen(giant_comp)
+
 
