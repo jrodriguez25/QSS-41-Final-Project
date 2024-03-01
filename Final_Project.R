a4701519c29ee9c2e148d3dd53f3d73f3ff0b39c
Jeremy Rodriguez <113404239+jrodriguez25@users.noreply.github.com>
2024-03-01 12:01:36 -0500

    Update Final_Project.R

--- Final_Project.R
+++ Final_Project.R
@@ -1,9 +1,9 @@
-library(igraph)
+library(igraph)   
 library(tidyverse)
 library(disparityfilter)
 
-edges <- read.csv("~//QSS41_proj/Data/edges.csv")
-nodes <- read.csv("~//QSS41_proj/Data/nodes.csv")
+nodes <- read_csv("SpotifyNetworkNodes.csv")
+edges <- read_csv("SpotifyNetworkEdges.csv")
 
 
 #remove duplicates
@@ -21,7 +21,7 @@ genres=nodes %>%
 
 # FILTER OUT GENRES -------------------------------------------------------
 nodes_filtered <- nodes %>% 
-  filter(popularity>70) %>% 
+  filter(popularity>70) %>%  # i think we need to remove this populatiry filter
   filter(!str_detect(genres, "classic") & !str_detect(genres, "jazz"))
 
 # CREATE GRAPH --------------------------------------------------------
@@ -37,51 +37,25 @@ network_graph <- graph_from_data_frame(d = cleaned_edges, vertices = nodes_filte
 
 #SIMPLIFIED GRAPH
 network_graph_simplified <- simplify(network_graph)
-
+V(network_graph_simplified) # 151678 vertices
 
 # LARGEST COMPONENT -----------------------------------------------
 decomp = components(network_graph_simplified)
 largest_comp = which.max(decomp$csize)
 large_ids = V(network_graph_simplified)[decomp$membership == largest_comp]
 giant_comp = induced_subgraph(network_graph_simplified, large_ids)
-
+V(giant_comp) # this is only 136287 vertices
 # PRUNING   ----------------------------------------------------------------
-
-#Generate z1 – the normalized weight of each edge from the perspective of empl1
-inventors.simp$z1 = (inventors.simp$collaborations-inventors.simp$mean.collabs1)/inventors.simp$sd.collabs1
 
 # K-Core Composition ------------------------------------------------------
 network.core <- coreness(network_graph_simplified,  mode = "all")
 network_core_df <- data.frame(node = names(network.core), coreness = network.core)
 
 
-#Weighting the edges for unweighted graphs
-network_simplified.adj = get.adjacency(network_graph_simplified, sparse = F)
-network_simplified.2 = network_simplified.adj %*% network_simplified.adj
-diag(network_simplified.2) = 0
-network_simplified.2[network_simplified.adj ==0]=0
-
-#Convert it to an edgelist
-network_simplfied.g2 = graph.adjacency(network_simplified.2, weighted = TRUE)
-network_simplified.weight = as.data.frame(cbind(get.edgelist(network_simplfied.g2), E(network_simplfied.g2)$weight))
-#rename
-names(network_simplified.weight) = c("artist1", "artist2", "weight")
-
-#Serrano Method Pruning NOT COMPLETE--------------------------------------------------
-
-#Serrano method
-network_weights.g = graph.edgelist(as.matrix(network_simplified.weight[,1:2]))
-E(network_weights.g)$weight <- as.numeric(network_simplified.weight[,3])
-
-E(network_weights.g)$weight
-
-network_weights.b = backbone(network_weights.g, weights = E(network_weights.g)$weight, alpha= .5)
-
-str(network_weights.b)
 
 
 
-# Flexible Backbone Pruning NOT COMPLETE -----------------------------------------------
+# Flexible Backbone Pruning COMPLETE -----------------------------------------------
 #paths of distance 2
 collabs.adj = get.adjacency(network_graph_simplified, sparse = F)
 collabs.2 = collabs.adj %*% collabs.adj
@@ -95,7 +69,6 @@ collabs.weight = as.data.frame(cbind(get.edgelist(collabs.g2), E(collabs.g2)$wei
 #rename
 names(collabs.weight) = c("artist1", "artist2", "weight")
 collabs.weight$weight <- as.numeric(collabs.weight$weight) # make numeric
-
 
 #Merge the mean and sd of collaborations for each artist1 to the edgelist
 mean.collabs = aggregate(collabs.weight$weight, list(collabs.weight$artist1), FUN = mean)
@@ -120,25 +93,64 @@ names(collabs.weight)[8] = "sd.collabs2"
 collabs.weight$z2 = (collabs.weight$weight-collabs.weight$mean.collabs2)/collabs.weight$sd.collabs2
 
 #We can now write our own rules in terms of z-scores. -> we need to mess around with these weight!!
-collabs.prune = collabs.weight[collabs.weight$z1 > 2 | collabs.weight$z2 > 2,]
+collabs.prune = collabs.weight[collabs.weight$z1 > 1 | collabs.weight$z2 > 1,]
 collabs.prune = collabs.prune[!is.na(collabs.prune$z1), 1:3]
 
 collabsgraph.prune <- graph.data.frame(collabs.prune)
 plot(collabsgraph.prune)
+V(collabsgraph.prune) # 457 vertices w/ 2, 1.3, 183 vertices w/ 2, 2. 401 vertices with 1.5, 1.5. 1 and 1 give 522 vert
+
 # STRUCTURAL SIMILAIRITY--NOT COMPLETE ---------------------------------------------------
 
+#adjacency matrix
 
-#want to set up similar distance matrix as slides
-euclidean_matrix <- as.matrix(dist(network_simplified.adj, method = "euclidean"))
-euclidean_matrix <- cbind()
+collabs.prune_adjacency <- as_adjacency_matrix(collabsgraph.prune, sparse = FALSE)
+
+# #adding followers and popularity to adjacency matrix
+# colnames(collabs.prune_adjacency)[1] <- "Artist"
+# collabs.prune_adjacency$followers <- nodes_filtered$followers
+# collabs.prune_adjacency$popularity <- nodes_filtered$popularity
 
 
-#looking at communities
-fg <- cluster_fast_greedy(giant_comp)
-plot(fg, giant_comp)
+# #change all numbers to be between 0 and 1
+# collabs.prune_adjacency$followers <- (collabs.prune_adjacency$followers - min(collabs.prune_adjacency$followers)) / (max(collabs.prune_adjacency$followers) - min(collabs.prune_adjacency$followers))
+# collabs.prune_adjacency$popularity <- (collabs.prune_adjacency$popularity - min(collabs.prune_adjacency$popularity)) / (max(collabs.prune_adjacency$popularity) - min(collabs.prune_adjacency$popularity))
 
-layout <- layout_with_eigen(giant_comp)
+collabs.dist <- dist(collabs.prune_adjacency, method = "euclidean", diag = TRUE, upper = TRUE)
+distance_matrix_df <- as.data.frame(as.matrix(collabs.dist))
 
+#Hierarchical Clustering from class
+
+collabs.dend = hclust(collabs.dist, method = "single")
+plot(collabs.dend)
+
+
+
+
+
+# BRIDGES AND WEAK TIES -----------------------------------------------------------
+
+decomp = components(collabsgraph.prune)
+largest_comp = which.max(decomp$csize)
+members = V(collabsgraph.prune)[decomp$membership == largest_comp]
+giant_comp = induced_subgraph(collabsgraph.prune, members)
+bridges(giant_comp) # no strict bridges
+print(V(giant_comp)) # gives 456 vertices
+
+
+#looking at local bridges
+prunedcollabs.adj = as_adj(collabsgraph.prune, sparse = F)
+prunedcollabs.adj.2 = prunedcollabs.adj %*% prunedcollabs.adj
+diag(prunedcollabs.adj.2) = 0
+prunedcollabs.adj.2[prunedcollabs.adj.2 == 0 & prunedcollabs.adj == 1] = -1
+prunedcollabs.adj.2[prunedcollabs.adj.2 != -1] = 0
+prunedcollabs.adj.2 = prunedcollabs.adj.2 * -1
+bridgegraph = graph.adjacency(prunedcollabs.adj.2)
+bridgegraph.edge = get.data.frame(bridgegraph)
+
+#count appearances in either to or from column
+counts <- table(c(bridgegraph.edge$from, bridgegraph.edge$to))
+print(counts)/2
 
 #test: getting rid of NAs: dom't need rn storage spaceeeeeee
 collabs.weight$weight[!is.na(collabs.weight$weight)]
